e05e14e3b9d70e56288ce63c312b4e29520c2193
jeebuscrossaint <thediamond270@gmail.com>
2026-02-28 00:05:00 -0500

    helllo lebron

--- linux/.config/coat/tty-theme.sh
+++ linux/.config/coat/tty-theme.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-# coat TTY theme: Firefox Dev
-# FredHappyface (https://github.com/fredHappyface)
+# coat TTY theme: Kanagawa Dragon
+# Stefan Weigl-Bosker (https://github.com/sweiglbosker), Tommaso Laurenzi (https://github.com/rebelot/kanagawa.nvim)
 # variant: dark
 
 # Apply Base16 colors to Linux TTY/console
 # This script should be run with root privileges or from /etc/profile.d/
 
 if [ "$TERM" = "linux" ]; then
-    printf '\033]P00B0D0D'
-    printf '\033]P1EE327D'
-    printf '\033]P23FC32F'
-    printf '\033]P30D69AB'
-    printf '\033]P428A1ED'
-    printf '\033]P5C170F3'
-    printf '\033]P64173A9'
-    printf '\033]P7A7AEAF'
-    printf '\033]P834494F'
-    printf '\033]P9EE327D'
-    printf '\033]PA3FC32F'
-    printf '\033]PB0D69AB'
-    printf '\033]PC28A1ED'
-    printf '\033]PDC170F3'
-    printf '\033]PE4173A9'
-    printf '\033]PFE6E6E6'
+    printf '\033]P0121111'
+    printf '\033]P1CD697D'
+    printf '\033]P2849E79'
+    printf '\033]P3CCC989'
+    printf '\033]P48AA8B6'
+    printf '\033]P5A293A7'
+    printf '\033]P68EA8A7'
+    printf '\033]P7C8CCC8'
+    printf '\033]P8605C58'
+    printf '\033]P9CD697D'
+    printf '\033]PA849E79'
+    printf '\033]PBCCC989'
+    printf '\033]PC8AA8B6'
+    printf '\033]PDA293A7'
+    printf '\033]PE8EA8A7'
+    printf '\033]PFC8CCC8'
     clear
 fi
