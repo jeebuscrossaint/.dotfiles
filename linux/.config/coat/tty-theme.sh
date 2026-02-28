bf583d5351d69009a0962a214f0c4c4b6c1779c2
jeebuscrossaint <thediamond270@gmail.com>
2026-02-27 20:01:47 -0500

    fornite

--- linux/.config/coat/tty-theme.sh
+++ linux/.config/coat/tty-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# coat TTY theme: Elementary
+# coat TTY theme: Firefox Dev
 # FredHappyface (https://github.com/fredHappyface)
 # variant: dark
 
@@ -7,21 +7,21 @@
 # This script should be run with root privileges or from /etc/profile.d/
 
 if [ "$TERM" = "linux" ]; then
-    printf '\033]P0181818'
-    printf '\033]P1D61B15'
-    printf '\033]P259A413'
-    printf '\033]P30855FE'
-    printf '\033]P4053A8C'
-    printf '\033]P5E40038'
-    printf '\033]P62594E0'
-    printf '\033]P7C5C5C5'
-    printf '\033]P8737373'
-    printf '\033]P9D61B15'
-    printf '\033]PA59A413'
-    printf '\033]PB0855FE'
-    printf '\033]PC053A8C'
-    printf '\033]PDE40038'
-    printf '\033]PE2594E0'
-    printf '\033]PF8C00EB'
+    printf '\033]P00B0D0D'
+    printf '\033]P1EE327D'
+    printf '\033]P23FC32F'
+    printf '\033]P30D69AB'
+    printf '\033]P428A1ED'
+    printf '\033]P5C170F3'
+    printf '\033]P64173A9'
+    printf '\033]P7A7AEAF'
+    printf '\033]P834494F'
+    printf '\033]P9EE327D'
+    printf '\033]PA3FC32F'
+    printf '\033]PB0D69AB'
+    printf '\033]PC28A1ED'
+    printf '\033]PDC170F3'
+    printf '\033]PE4173A9'
+    printf '\033]PFE6E6E6'
     clear
 fi
