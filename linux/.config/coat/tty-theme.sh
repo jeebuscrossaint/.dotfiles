75070ba867c54cf038da9557f0cc4a24e7201b36
jeebuscrossaint <thediamond270@gmail.com>
2026-03-01 23:39:23 -0500

    color

--- linux/.config/coat/tty-theme.sh
+++ linux/.config/coat/tty-theme.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-# coat TTY theme: Wallpaper
-# coat (extracted)
+# coat TTY theme: Material Dark
+# FredHappyface (https://github.com/fredHappyface)
 # variant: dark
 
 # Apply Base16 colors to Linux TTY/console
 # This script should be run with root privileges or from /etc/profile.d/
 
 if [ "$TERM" = "linux" ]; then
-    printf '\033]P02A2624'
-    printf '\033]P12A1F18'
-    printf '\033]P22A1F18'
-    printf '\033]P32A1F18'
-    printf '\033]P42A1F18'
-    printf '\033]P52A1F18'
-    printf '\033]P62A1F18'
-    printf '\033]P7ACA9A7'
-    printf '\033]P8797472'
-    printf '\033]P92A1F18'
-    printf '\033]PA2A1F18'
-    printf '\033]PB2A1F18'
-    printf '\033]PC2A1F18'
-    printf '\033]PD2A1F18'
-    printf '\033]PE2A1F18'
-    printf '\033]PFDEDFE0'
+    printf '\033]P01D1D1C'
+    printf '\033]P1AC183E'
+    printf '\033]P26B7C16'
+    printf '\033]P36591E9'
+    printf '\033]P41737A6'
+    printf '\033]P5480F69'
+    printf '\033]P60F4D6C'
+    printf '\033]P7C6C6C6'
+    printf '\033]P86C6C6C'
+    printf '\033]P9AC183E'
+    printf '\033]PA6B7C16'
+    printf '\033]PB6591E9'
+    printf '\033]PC1737A6'
+    printf '\033]PD480F69'
+    printf '\033]PE0F4D6C'
+    printf '\033]PFDCDCDC'
     clear
 fi
