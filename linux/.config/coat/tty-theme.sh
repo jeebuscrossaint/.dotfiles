8f9f5385c38fe53ee073932843f70db05daa42f2
jeebuscrossaint <thediamond270@gmail.com>
2026-03-05 17:19:15 -0500

    jbruh

--- linux/.config/coat/tty-theme.sh
+++ linux/.config/coat/tty-theme.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-# coat TTY theme: Charcoal Dark
-# Mubin Muhammad (https://github.com/mubin6th)
+# coat TTY theme: Default Dark
+# Chris Kempson (http://chriskempson.com)
 # variant: dark
 
 # Apply Base16 colors to Linux TTY/console
 # This script should be run with root privileges or from /etc/profile.d/
 
 if [ "$TERM" = "linux" ]; then
-    printf '\033]P00F0B05'
-    printf '\033]P1A88C62'
-    printf '\033]P2DEC8A7'
-    printf '\033]P3DEC8A7'
-    printf '\033]P4C3A983'
-    printf '\033]P5A88C62'
-    printf '\033]P6DEC8A7'
-    printf '\033]P7C3A983'
-    printf '\033]P857462C'
-    printf '\033]P9A88C62'
-    printf '\033]PADEC8A7'
-    printf '\033]PBDEC8A7'
-    printf '\033]PCC3A983'
-    printf '\033]PDA88C62'
-    printf '\033]PEDEC8A7'
-    printf '\033]PF231B0E'
+    printf '\033]P0181818'
+    printf '\033]P1AB4642'
+    printf '\033]P2A1B56C'
+    printf '\033]P3F7CA88'
+    printf '\033]P47CAFC2'
+    printf '\033]P5BA8BAF'
+    printf '\033]P686C1B9'
+    printf '\033]P7D8D8D8'
+    printf '\033]P8585858'
+    printf '\033]P9AB4642'
+    printf '\033]PAA1B56C'
+    printf '\033]PBF7CA88'
+    printf '\033]PC7CAFC2'
+    printf '\033]PDBA8BAF'
+    printf '\033]PE86C1B9'
+    printf '\033]PFF8F8F8'
     clear
 fi
